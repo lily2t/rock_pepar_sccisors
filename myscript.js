645eaafea1278faec8291b400530f667a119c4a7
lily2t <amit068@yahoo.com>
2022-05-10 23:37:16 +0200

    First updated version

--- myscript.js
+++ myscript.js
@@ -13,8 +13,53 @@ function computerPlay() {
 console.log(computerPlay());
 
 
-/*function playRound(playerSelection, computerSelection){
+function playRound(playerSelection, computerSelection){
 
-    if(playerSelection === )
-    return "Wow! You beat the Computer";
-}*/
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if(playerSelection === "rock"){
+        if(computerSelection === "rock"){
+            return "It is tie."
+        }else if(computerSelection === "scissors"){
+            return "Congrats, you won!";
+        }else if(computerSelection === "paper"){
+            return "You loose maybe next time."
+        }
+    }
+
+
+    if(playerSelection === "scissors"){
+        if(computerSelection === "scissors"){
+            return "It is tie."
+        }else if(computerSelection === "paper"){
+            return "Congrats, you won!";
+        }else if(computerSelection === "rock"){
+            return "You loose maybe next time."
+        }
+    }
+
+    if(playerSelection === "paper"){
+        if(computerSelection === "paper"){
+            return "It is tie."
+        }else if(computerSelection === "rock"){
+            return "Congrats, you won!";
+        }else if(computerSelection === "scissors"){
+            return "You loose maybe next time."
+        }
+    }
+
+
+}
+
+function game(){
+    for(let i= 0; i < 5; i++){
+        let playerSelect = window.prompt("Enter yor move");
+        computerSelection = computerPlay();
+        console.log(playRound(playerSelect, computerSelection));
+    }
+}
+
+    /*playerSelection = "rock";
+    computerSelection = computerPlay();
+    console.log(playRound(playerSelection, computerSelection));*/
